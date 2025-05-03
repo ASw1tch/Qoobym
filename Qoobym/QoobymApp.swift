77967ad88e6295c943be2420d9b38af65188fabc
Anatolii Petrov <hello@aswitch.me>
2025-05-03 21:07:42 +0200

    Initial commit

---
+++ Qoobym/QoobymApp.swift
@@ -0,0 +1,17 @@
+//
+//  QoobymApp.swift
+//  Qoobym
+//
+//  Created by Anatoliy Petrov on 3. 5. 2025..
+//
+
+import SwiftUI
+
+@main
+struct QoobymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
